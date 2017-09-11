cc61cc9ec3663a1be0b65a2cfd78f2ca5fd635c3
aschwinwester <aschwin.wester@gmail.com>
2017-09-11 21:55:07 +0200

    Update exercise 1_5.py
    Klein foutje.

--- exercise 1_5.py
+++ exercise 1_5.py
@@ -1,6 +1,8 @@
 favorieten = ['Sean Paul']
 print(favorieten + ['David Guetta', 'Katy Perry'])
 list = ['Sean Paul', 'David Guetta', 'Katy Perry']
+
+# favorieten is nog steeds lengte 1, de volgende statement geeft dus een fout.
 favorieten[1] = 'afrojack'
 
 
